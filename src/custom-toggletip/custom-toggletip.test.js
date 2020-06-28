6a6ace321cc30864283fae9fcaa3dc7e336ec14c
Eli Howey <EliHowey@users.noreply.github.com>
2020-06-27 23:42:36 -0400

    test(custom-toggletip): :white_check_mark: add tests for Toggletip (#2)
    * chore: :wrench: set up Karma testing suite
    
    * test(custom-toggletip): :white_check_mark: add tests for Toggletip
    
    * chore: :art: add newline to end of .gitignore

---
+++ src/custom-toggletip/custom-toggletip.test.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { expect, fixture } from '@open-wc/testing';
+import { Toggletip } from './custom-toggletip.js';
+
+customElements.define('custom-toggletip', Toggletip);
+
+const template = `
+	<custom-toggletip>
+		<span slot="toggle">Toggle button</span>
+		This is the toggletip main content.
+	</custom-toggletip>
+`;
+
+/**
+ * @typedef {import('./custom-toggletip.js').Toggletip} Toggletip
+ */
+
+describe('Toggletip', () => {
+    it('is accessible when the toggle is collapsed', async () => {
+        const el = /** @type {Toggletip} */ (await fixture(template));
+        expect(el).to.be.accessible();
+    });
+
+    it('is accessible when the toggle is expanded', async () => {
+        const el = /** @type {Toggletip} */ (await fixture(template));
+        el.dispatchEvent(new Event('click'));
+        expect(el).to.be.accessible();
+    });
+
+    it('updates its toggled state when clicked', async () => {
+        const el = /** @type {Toggletip} */ (await fixture(template));
+        expect(el._toggleButton.getAttribute('aria-expanded')).to.equal(
+            'false'
+        );
+
+        el.dispatchEvent(new Event('click'));
+        expect(el._toggleButton.getAttribute('aria-expanded')).to.equal('true');
+    });
+});
